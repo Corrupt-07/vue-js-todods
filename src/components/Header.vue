6faedbcf831ee04ecd23405bbba6ce0b6b08384a
Corrupt-07 <harshverenkar67@gmail.com>
2024-06-24 17:37:12 +0530

    second changes

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+     <h1>{{ title }}</h1>
+     <Button text="Add Task" color="green"/>
+     <!-- <Button text="Update Task" color="blue"/>
+     <Button text="Delete Task" color="red"/> -->
+  </header>
+  
+</template>
+
+<script>
+import Button from './Button.vue'
+
+export default {
+  name: 'Navbar',
+  props: {
+    title: String,
+  },
+  components:{
+    Button
+  }
+}
+</script>
+
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
